b9e035093574b8f1cc98758999454094c23f8cc1
Davide <davidevischetti@gmail.com>
2022-06-07 15:33:38 +0200

    esercizio senza bonus

---
+++ src/components/NewTitle.vue
@@ -0,0 +1,16 @@
+<!-- HTML -->
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<!-- JS -->
+<script>
+export default {
+  name: 'NewTitle'
+}
+</script>
+
+<!-- CSS -->
+<style scoped lang="scss">
+
+</style>
